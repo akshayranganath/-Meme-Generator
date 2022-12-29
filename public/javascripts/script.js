d34b636a68c26c6ea8044caf546d4002544a65b4
Akshay Ranganath <akshay.ranganath@cloudinary.com>
2022-12-29 12:17:25 -0800

    added the code to support the use of generated images

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,8 +1,13 @@
 function generateImageOverlay (publicId, message) {
   publicId = publicId || 'subhashitaani/sky-space-dark-galaxy'
+  let method = 'upload'
+  // check if public id is a URL
+  if(publicId.startsWith('https://res.cloudinary.com/')){
+    method = 'fetch'
+  }
   message = 'उद्यमेन हि सिध्यन्ति कार्याणि न मनोरथैः|\nन हि सुप्तस्य सिंहस्य प्रविशन्ति मुखे मृगाः||'
   message = encodeURIComponent(message)
-  const base = 'https://res.cloudinary.com/dbmataac4/image/upload/w_1024,h_768/'
+  const base = `https://res.cloudinary.com/dbmataac4/image/${method}/w_1024,h_768/`
   const themes = [
       `l_text:Mukta_60:${message}`,
       `co_rgb:FFFF00,l_text:Mukta_60:${message}`,
@@ -38,4 +43,19 @@ function generateImageOverlay (publicId, message) {
   // finall display the div
   const previewDiv = document.getElementById('preview')
   previewDiv.style.display = 'block'
+}
+
+
+function getImage(textPrompt){
+  const url = `https://hi.final-tou.ch/_/generate/greeting?scene=12&superlative=${encodeURIComponent(textPrompt)}&artifacts=&batch_size=1&style=3d,%20octane%20render,%20depth%20of%20field,%20unreal%20engine%205,%20concept%20art%20of%20a%20(({})),%20vibrant%20colors,%20glow,%20trending%20on%20artstation,%20ultra%20high%20detail,%20ultra%20realistic,%20cinematic%20lighting,%20focused,%2012k`
+  fetch(url)
+  .then(resp => {
+    if(!resp.ok) {
+      throw new Error(`HTTP error, status = ${response.status}`);
+    }
+    return resp.json()
+  })
+  .then(data => {
+    console.log(JSON.stringify(data))
+  })
 }
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express')
+const fetch = require('node-fetch')
 var router = express.Router()
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -7,7 +9,27 @@ router.get('/', function (req, res, next) {
 })
 
 router.get('/edit', function (req, res, next) {
+  //res.end('Hello, World!');
   res.render('edit', { title: 'सुभाषितम्' })
 })
 
+router.get('/generative', function (req, res, next) {
+  res.render('generative', { title: 'सुभाषितम्' })
+})
+
+router.get('/getImage', function(req, res, next){
+  const keyword = req.query.keyword || 'sunrise'
+  
+  const url = `https://hi.final-tou.ch/_/generate/greeting?scene=12&superlative=${keyword}&artifacts=&batch_size=1&style=3d,%20octane%20render,%20depth%20of%20field,%20unreal%20engine%205,%20concept%20art%20of%20a%20(({})),%20vibrant%20colors,%20glow,%20trending%20on%20artstation,%20ultra%20high%20detail,%20ultra%20realistic,%20cinematic%20lighting,%20focused,%2012k`
+  fetch(url)
+  .then(resp => {
+    if(!resp.ok) {
+      throw new Error(`HTTP error, status = ${response.status}`);
+      }
+      return resp.json()
+  })
+  .then(data => {
+    res.json(data)
+  })
+})
 module.exports = router
